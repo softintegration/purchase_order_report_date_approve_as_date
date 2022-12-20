78d3ad79b767d3dfca5dbba63db63516507f5357
Mossaab BOUDCHICHA <mossaab.boudchicha@gmail.com>
2022-12-20 10:05:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Purchase order confirmation date as the purchase order date in report
+---------------------------------------------------------------------
+This module set the Confirmation date of the Purchase Order as the date displayed in the report Purchase Order Date.
+
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+ 'name': 'Purchase order confirmation date as the purchase order date in report',
+ 'author': 'Soft-integration',
+ 'application': False,
+ 'installable': True,
+ 'auto_install': False,
+ 'qweb': [],
+ 'description': False,
+ 'images': [],
+ 'version': '1.0.1',
+ 'category': 'Purchase',
+ 'demo': [],
+ 'depends': ['purchase'],
+ 'data': [
+    'report/purchase_order_templates.xml'
+    ],
+ 'license': 'LGPL-3',
+ }
